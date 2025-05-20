6ab49b7ce737999123c200fcc448fb9c138a95fa
Ancuta <onutaancuta@gmail.com>
2025-05-20 11:15:53 +0200

    small updates

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,16 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 import { Database } from "@/types/supabase";
 
-if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+if (!process.env.SUPABASE_URL) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
 }
-if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+if (!process.env.SUPABASE_ANON_KEY) {
   throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
 }
 
 export const supabase = createClient<Database>(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_ANON_KEY
 );
 
 export default supabase;
